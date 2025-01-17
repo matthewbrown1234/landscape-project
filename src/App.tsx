53d4d1217ca0b93cae33cca087acb0829066d406
Matthew Brown <matt.d.brown.7@gmail.com>
2025-01-16 17:15:50 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,333 @@
+import React from "react";
+import Legend from "./Legend.tsx";
+
+const convertFeetToPixels = (feet: number) => (feet * 30);
+
+const showLongRulers = true
+
+const Circle = (props: { cx: number, cy: number, d: number, label: string }) => {
+  const {cx, cy, d} = props;
+  return (<>
+        <circle cx={cx} cy={cy} r={d * .5} stroke={"black"} strokeWidth={1}/>
+        <circle cx={cx} cy={cy} r={3} fill={"black"}/>
+        <text fill="#000000" fontSize="12"
+              x={cx - 15} y={cy + 15}>{props.label}
+        </text>
+      </>
+  )
+}
+
+function App() {
+  const width = 56; // ft
+  const height = 29.4; // ft
+
+  const leftBufferInFeet = 4;
+
+
+  const yCords = [];
+  let y = convertFeetToPixels(height);
+  while (y >= 0) {
+    yCords.push(y);
+    y = y - 30
+  }
+
+  const xCords = [];
+  let x = 100;
+  while (x <= 1790) {
+    xCords.push(x);
+    x = x + 30
+  }
+
+  return (
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <h1>
+          Garden Plan
+        </h1>
+        <div
+            style={{
+              width: "100%"
+            }}>
+          <h2>Plan 1</h2>
+          <div style={{
+            paddingRight: "5px",
+            paddingLeft: "5px"
+          }}>
+            <svg
+                viewBox={`0 0 ${convertFeetToPixels(width + leftBufferInFeet)} ${convertFeetToPixels(height + 4)}`}
+                xmlns="http://www.w3.org/2000/svg">
+              <rect x="0" y="0" width={convertFeetToPixels(width + leftBufferInFeet)}
+                    height={convertFeetToPixels(height + 2.5)}
+                    fill={"#c0c0c0"}/>
+              <text fill="#000000" fontSize="12"
+                    x="3" y="500">driveway
+              </text>
+
+
+              <rect x={100} width={convertFeetToPixels(width + 3.4) - 100}
+                    height={convertFeetToPixels(height)}
+                    fill={"#5a4a36"}/>
+
+              <rect x={900} width={convertFeetToPixels(width + 3.4) - 100}
+                    height={convertFeetToPixels(height)}
+                    fill={"green"}/>
+
+              <ellipse cx="1200" cy="600" fill={"green"} name={"grass"} rx="470" ry="600"/>
+
+              <rect x={200} y={792} width={convertFeetToPixels(width)}
+                    height={convertFeetToPixels(3)}
+                    fill={"#5a4a36"}
+                    name={"mulch"}
+              />
+
+              <rect x={650} y={650} width={convertFeetToPixels(7)}
+                    height={convertFeetToPixels(5)}
+                    fill={"#5a4a36"}
+                    name={"mulch"}
+              />
+              <ellipse cx="1121" cy="640" fill={"green"} name={"grass"} rx="390" ry="150"/>
+
+              <rect x="100" y={220} width={convertFeetToPixels(14.3)}
+                    height={convertFeetToPixels(4)} fill={"#c0c0c0"}/>
+              <text fill="#000000" fontSize="12"
+                    x="340" y="310">sidewalk
+              </text>
+              <rect x="409" y={0} width={convertFeetToPixels(4)}
+                    height={convertFeetToPixels(height)} fill={"#c0c0c0"}/>
+              <rect x="0" y={882} width={convertFeetToPixels(width + leftBufferInFeet)}
+                    height={convertFeetToPixels(4)} fill={"#c0c0c0"}/>
+              <text fill={"#000000"} fontSize="12"
+                    x="340" y="930">sidewalk
+              </text>
+
+
+              <g fill={"#453232"}>
+                <Circle cx={185} cy={590 + 267} d={convertFeetToPixels(1.25)} label={"rock"}/>
+                <Circle cx={380} cy={580 + 267} d={convertFeetToPixels(1.25)} label={"rock"}/>
+              </g>
+
+              <g fill={"#5f744b"}>
+                {/*https://shop.bathgardencenter.com/item/055210081028/Pine-Slowmound-Mugo-Pinus-mugo-Slowmound-2-gallon?cid=25*/}
+                {/*https://www.waterwiseplants.org/find-a-plant/mops-dwarf-mugo-pine/#header*/}
+                <Circle cx={600} cy={770} d={convertFeetToPixels(3)} label={"Mugo Pine"}/>
+              </g>
+              <g fill={"#cd3d33"}>
+                {/* 6" tall */}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/zauschneria-garrettii-orange-carpet*/}
+                <Circle cx={275} cy={95 + 267} d={convertFeetToPixels(1.5)}
+                        label={"Orange Carpet"}/>
+                <Circle cx={230} cy={95 + 267} d={convertFeetToPixels(1.5)}
+                        label={"Orange Carpet"}/>
+                <Circle cx={185} cy={95 + 267} d={convertFeetToPixels(1.5)}
+                        label={"Orange Carpet"}/>
+                <Circle cx={205} cy={130 + 267} d={convertFeetToPixels(1.5)}
+                        label={"Orange Carpet"}/>
+                <Circle cx={250} cy={130 + 267} d={convertFeetToPixels(1.5)}
+                        label={"Orange Carpet"}/>
+              </g>
+              <g fill={"#bda7ee"}>
+                {/* 10" tall */}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/nepeta-little-trudy-catmint*/}
+                <Circle cx={310} cy={95 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={350} cy={95 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={390} cy={95 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={115} cy={230 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={145} cy={250 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={115} cy={260 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={145} cy={280 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+                <Circle cx={115} cy={290 + 267} d={convertFeetToPixels(1.33)} label={"Catmint"}/>
+              </g>
+
+              <g fill={"#fde933"}>
+                {/* 18" tall*/}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/achillea-moonshine*/}
+                <Circle cx={175} cy={350 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={130} cy={340 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={130} cy={380 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={380} cy={210 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={340} cy={220 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={380} cy={260 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={380} cy={415 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={340} cy={435 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+                <Circle cx={380} cy={470 + 267} d={convertFeetToPixels(2)} label={"Yarrow"}/>
+              </g>
+
+              {/*<g fill={"#6a454a"}>*/}
+              {/*  /!* 30" tall *!/*/}
+              {/*  /!*https://www.waterwiseplants.org/find-a-plant/crimson-pygmy-barberry/#header*!/*/}
+              {/*  <Circle cx={150} cy={140 + 267} d={convertFeetToPixels(2)}*/}
+              {/*          label={"bush"}/>*/}
+              {/*</g>*/}
+
+
+              {/*<g fill={"#b7cebf"}>*/}
+              {/*  /!* 30" tall *!/*/}
+              {/*  /!*https://www.highcountrygardens.com/product/perennial-plants/artemisia-powis-castle*!/*/}
+              {/*  <Circle cx={145} cy={165 + 267} d={convertFeetToPixels(3)}*/}
+              {/*          label={"powis Castle artemisia"}/>*/}
+              {/*</g>*/}
+
+
+              {/*<g fill={"#aeb429"}>*/}
+              {/*  /!*https://www.highcountrygardens.com/product/perennial-plants/solidago-sphacelata-golden-fleece*!/*/}
+              {/*  <Circle cx={190} cy={520 + 267} d={convertFeetToPixels(2)} label={"Goldenrod"}/>*/}
+              {/*  <Circle cx={380} cy={400 + 267} d={convertFeetToPixels(2)} label={"Goldenrod"}/>*/}
+              {/*</g>*/}
+
+              <g fill={"#ffffff"}>
+                <Circle cx={590} cy={310 + 267} d={convertFeetToPixels(1.25)} label={"Pow Wow"}/>
+              </g>
+              <g fill={"#ff7157"}>
+                {/*https://www.highcountrygardens.com/product/perennial-plants/penstemon-eatonii-richfield*/}
+                <Circle cx={620} cy={180 + 267} d={convertFeetToPixels(1.25)} label={"Penstemon"}/>
+              </g>
+
+              <g fill={"#b2305b"}>
+                {/*https://www.highcountrygardens.com/product/perennial-plants/centranthus-ruber-coccineus*/}
+                <Circle cx={150} cy={180 + 267} d={convertFeetToPixels(2.5)} label={"Valerian"}/>
+                <Circle cx={300} cy={390 + 267} d={convertFeetToPixels(2.5)} label={"Valerian"}/>
+              </g>
+
+              <g fill={"#fffb00"}>
+                <Circle cx={700} cy={190 + 267} d={convertFeetToPixels(2)} label={"BES"}/>
+                <Circle cx={680} cy={280 + 267} d={convertFeetToPixels(2)} label={"BES"}/>
+                <Circle cx={670} cy={360 + 267} d={convertFeetToPixels(2)} label={"BES"}/>
+                <Circle cx={695} cy={440 + 267} d={convertFeetToPixels(2)} label={"BES"}/>
+                <Circle cx={760} cy={500 + 267} d={convertFeetToPixels(2)} label={"BES"}/>
+              </g>
+
+
+              <g fill={"#aea3d5"}>
+                {/* 48" tall */}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/perovskia-atriplicifolia-blue-spires*/}
+                <Circle cx={220} cy={215 + 267} d={convertFeetToPixels(4)} label={"russian sage"}/>
+                <Circle cx={260} cy={450 + 267} d={convertFeetToPixels(4)} label={"russian sage"}/>
+                <Circle cx={620} cy={105 + 267} d={convertFeetToPixels(4)} label={"russian sage"}/>
+              </g>
+
+              <g fill={"#ffffff"}>
+                <Circle cx={700} cy={285 + 267} d={10} label={""}/>
+              </g>
+
+
+              <g fill={"#a8b298"}>
+                {/*<Circle cx={150} cy={250 + 267} d={convertFeetToPixels(2)} label={"Blue Oat Grass"}/>*/}
+              </g>
+
+              {/*<path d="M350 610 Q400 480 470 610 Q520 480 600 610" fill={"#6c9827"}/>*/}
+              {/*<text fill="#000000" fontSize="12"*/}
+              {/*      x={490} y={590}>ground cover*/}
+              {/*</text>*/}
+              <g fill={"#c766d8"}>
+                {/* 6" tall */}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/purple-beauty-creeping-phlox*/}
+                {/*<Circle cx={555} cy={390 + 267} d={convertFeetToPixels(2)} label={"phlox"}/>*/}
+                {/*<Circle cx={595} cy={400 + 267} d={convertFeetToPixels(2)} label={"phlox"}/>*/}
+                {/*<Circle cx={555} cy={440 + 267} d={convertFeetToPixels(2)} label={"phlox"}/>*/}
+              </g>
+              <g fill={"#bea4e2"}>
+                {/*https://www.highcountrygardens.com/product/perennial-plants/echinops-banaticus-blue-glow*/}
+                <Circle cx={200} cy={435 + 267} d={convertFeetToPixels(1.5)} label={"echinops"}/>
+                <Circle cx={220} cy={350 + 267} d={convertFeetToPixels(1.5)} label={"echinops"}/>
+                <Circle cx={260} cy={270 + 267} d={convertFeetToPixels(1.5)} label={"echinops"}/>
+              </g>
+              <g fill={"#8e3eff"}>
+                {/* 18" tall */}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/lavandula-angustifolia-vera*/}
+                <Circle cx={130} cy={435 + 267} d={convertFeetToPixels(2)} label={"lavender"}/>
+                <Circle cx={180} cy={490 + 267} d={convertFeetToPixels(2)} label={"lavender"}/>
+                <Circle cx={130} cy={485 + 267} d={convertFeetToPixels(2)} label={"lavender"}/>
+                <Circle cx={180} cy={545 + 267} d={convertFeetToPixels(2)} label={"lavender"}/>
+                <Circle cx={130} cy={535 + 267} d={convertFeetToPixels(2)} label={"lavender"}/>
+                <Circle cx={130} cy={585 + 267} d={convertFeetToPixels(2)} label={"lavender"}/>
+              </g>
+              <g fill={"#f672dd"}>
+                {/*https://www.highcountrygardens.com/product/perennial-plants/callirhoe-involucrata*/}
+                <Circle cx={230} cy={570 + 267} d={convertFeetToPixels(2.5)} label={"purple mallow"}/>
+                <Circle cx={280} cy={570 + 267} d={convertFeetToPixels(2.5)} label={"purple mallow"}/>
+                {/*<Circle cx={250} cy={520 + 267} d={convertFeetToPixels(2.5)} label={"purple mallow"}/>*/}
+              </g>
+              <g fill={"#d9bec9"}>
+                {/* 24" tall */}
+                {/*https://www.highcountrygardens.com/product/perennial-plants/aster-oblongifolius-raydons-favorite*/}
+                {/*<Circle cx={560} cy={405 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>*/}
+                {/*<Circle cx={605} cy={435 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>*/}
+                {/*<Circle cx={560} cy={465 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>*/}
+                <Circle cx={380} cy={310 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>
+                <Circle cx={335} cy={340 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>
+                <Circle cx={380} cy={360 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>
+                <Circle cx={380} cy={520 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>
+                <Circle cx={330} cy={505 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>
+                <Circle cx={335} cy={560 + 267} d={convertFeetToPixels(2)} label={"Asters"}/>
+              </g>
+              {/*<g fill={"#8782d8"}>*/}
+              {/*  <Circle cx={330} cy={585 + 267} d={convertFeetToPixels(1.5)} label={"Echinops"}/>*/}
+              {/*</g>*/}
+              <g fill={"#e8dbaa"}>
+                {/*https://www.provenwinners.com/plants/nassella-formerly-stipa/mexican-feather-grass-nassella-formerly-stipa-tenuissima*/}
+                <Circle cx={300} cy={260 + 267} d={convertFeetToPixels(2)}
+                        label={"Mexican Feather Grass"}/>
+                <Circle cx={280} cy={340 + 267} d={convertFeetToPixels(2)}
+                        label={"Mexican Feather Grass"}/>
+                <Circle cx={210} cy={290 + 267} d={convertFeetToPixels(2)}
+                        label={"Mexican Feather Grass"}/>
+                <Circle cx={190} cy={390 + 267} d={convertFeetToPixels(2)}
+                        label={"Mexican Feather Grass"}/>
+              </g>
+
+
+              <g fill={"#429795"}>
+                {/*https://shop.bathgardencenter.com/item/055209090017/Blue-Spruce-Dwarf-Globe-Picea-pungens-Glauca-Globosa-1-gallon?cid=25*/}
+                <Circle cx={760} cy={150} d={convertFeetToPixels(6)}
+                        label={"dwarf globe blue spruce"}/>
+              </g>
+
+              {/* vert rule*/}
+              <line x1="80" y1="0" x2="80" y2="881" stroke="black" strokeWidth="1"/>
+              <g stroke="black" strokeWidth="1">
+                {
+                  yCords.map((yCords, index) => (
+                      <React.Fragment key={index}>
+                        <line x1="75" y1={yCords}
+                              x2={index % 2 === 0 && showLongRulers ? 1789 : 95}
+                              y2={yCords}/>
+                        <line x1="1770" y1={yCords}
+                              x2={1789}
+                              y2={yCords}/>
+                      </React.Fragment>
+                  ))
+                }
+              </g>
+              <g fontSize="10">
+                {
+                  yCords.map((y, index) =>
+                      <text key={index} x="61" y={y + 2}>{index}'</text>
+                  )
+                }
+              </g>
+              {/*  hor rule */}
+              <g stroke="black" strokeWidth="1">
+                {xCords.map((x, index) => (
+                    <React.Fragment key={index}>
+                      <line x1={x} y1="0" x2={x}
+                            y2={index % 2 === 0 && showLongRulers ? 870 : 15}
+                      />
+                      <line x1={x} y1="890" x2={x} y2={870}/>
+                    </React.Fragment>
+                ))}
+              </g>
+              <g fontSize="10">
+                {
+                  xCords.map((x, index) =>
+                      <text key={index} x={x - 2} y={"910"}>{index}'</text>
+                  )
+                }
+              </g>
+            </svg>
+          </div>
+        </div>
+        <Legend/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Legend.tsx
@@ -0,0 +1,262 @@
+
+export default function Legend () {
+  return <div style={{
+    display: "flex",
+    flexDirection: "row",
+    padding: "0 7px",
+  }}>
+    <div style={{
+      maxWidth: "350px",
+      padding: "5px",
+      background: "beige",
+      borderRadius: "7px",
+      lineHeight: "1.25",
+      height: "max-content"
+    }}>
+      <h2 style={{textAlign: "center"}}>Legend</h2>
+      <ul style={{fontSize: "smaller"}}>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/achillea-moonshine"}>
+            Yarrow
+          </a>
+          <p>
+            <i>
+              * 18" tall x 24" wide <br/>
+              * blooms all summer<br/>
+              * $10.39 each<br/>
+              * cut back in mid spring
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/aster-oblongifolius-raydons-favorite"}>
+            Raydon's Favorite Aromatic Aster
+          </a>
+          <p>
+            <i>
+              * 36" tall x 24" wide <br/>
+              * Early to late fall<br/>
+              * $10.39 each<br/>
+              * Leave standing over the winter months and cut back hard to 1 to 2" above the ground.
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/centranthus-ruber-coccineus"}>
+            Valerian - Red Valerian
+          </a>
+          <p>
+            <i>* 36" tall x 30" wide <br/>
+              * late spring and blooms all summer<br/>
+              * $10.39 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/nepeta-little-trudy-catmint"}>
+            Catmint - Little Trudy Catmint
+          </a>
+          <p>
+            <i>* 10" tall x 16" wide <br/>
+              * purple late spring early fall<br/>
+              * $10.39 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/zauschneria-garrettii-orange-carpet"}>
+            Orange Carpet® Creeping Hummingbird Trumpet (Zauschneria)
+          </a>
+          <p>
+            <i>* 6" tall x 18" wide <br/>
+              * orange summer<br/>
+              * $11.19 each<br/>
+              * Leave standing over the winter. Cut back to just above ground level in mid-spring.<br/>
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/bouteloua-gracilis-blonde-ambition"}>
+            Blonde Ambition Blue Grama Grass
+          </a>
+          <p>
+            <i>* 36" tall x 36" wide <br/>
+              * mid to late summer<br/>
+              * $7.99 each<br/>
+              * Cutting back Blonde Ambition Blue Grama Grass (Bouteloua gracilis Blonde Ambition) should be done in mid-spring when the new green grass blades begin to sprout from the crown. The stems holding the seed heads are very resilient and stay upright even after a snowy winter, so the grass looks good until mid-spring. Cut back to a height of 2 to 3 inches above ground level and scratch out the crown with gloved hands to loosen thatch and make room for the new growth to push up and out.<br/>
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/artemisia-powis-castle"}>
+            Artemisia powis castle
+          </a>
+          <p>
+            <i>* 30" tall x 36" wide<br/>
+              * $10.39<br/>
+              * Prune back to 6" tall in mid-spring to reinvigorate plant each year.
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/lavandula-angustifolia-vera"}>
+            Vera English Lavender
+          </a>
+          <p>
+            <i>* 30" tall x 36" wide<br/>
+              * $12.79<br/>
+              * purple summer<br/>
+              * Prune Lavender plants in spring as needed.  When plants begin to show signs of new growth, cut back the old stems by no more than one-third to re-invigorate the plant and encourage more flowers. I recommend waiting until mid-spring to prune out any winter-damaged branches, and gently shear off a few inches of the branch tips to shape a round, mounded plant.
+            </i>
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div style={{
+      maxWidth: "350px",
+      width: "350px",
+      padding: "5px",
+      background: "beige",
+      borderRadius: "7px",
+      lineHeight: "1.25",
+      marginLeft: "15px",
+      marginRight: "15px",
+      maxHeight: "max-content"
+    }}>
+      <h2 style={{textAlign: "center"}}>Cost</h2>
+      <ul style={{fontSize: "smaller"}}>
+        <li>
+          Yarrow <br/>
+          2 @ $10.39 each = $20.78
+        </li>
+        <li>
+          Penstemon - Firecracker Penstemon <br/>
+          2 @ $10.39 each = $20.78
+        </li>
+        <li>
+          Valerian - Red Valerian <br/>
+          3 @ $10.39 each = $31.17
+        </li>
+        <li>
+          Catmint - Little Trudy Catmint <br/>
+          2 @ $10.39 each = $20.78
+        </li>
+        <li>
+          BES - Black Eyed Susan <br/>
+          5 @ $10.39 each = $51.95
+        </li>
+        {/*<li>*/}
+        {/*  ground cover - creeping thyme <br/>*/}
+        {/*  2 @ $7.99 each = $15.98*/}
+        {/*</li>*/}
+        <li>
+          STG - Mexican Feather Grass aka Silky Thread Grass <br/>
+          5 @ $11.19 each = $55.95
+        </li>
+        <li>
+          Pow wow echinacea <br/>
+          3 @ $8.79 each = $26.37
+        </li>
+      </ul>
+      <span>
+              Total: $294.92 <br/>
+              mulch: ~$300 for 7 yards (+ delivery)
+            </span>
+    </div>
+    <div style={{
+      maxWidth: "350px",
+      padding: "5px",
+      background: "beige",
+      borderRadius: "7px",
+      lineHeight: "1.25",
+      height: "max-content"
+    }}>
+      <h2 style={{textAlign: "center"}}>Nice to haves</h2>
+      <ul style={{fontSize: "smaller"}}>
+        <li>
+          <a href={"https://www.waterwiseplants.org/find-a-plant/drumstick-allium/#header"}>
+            Drumstick Allium</a>
+          <p>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/penstemon-eatonii-richfield"}>
+            Penstemon - Firecracker Penstemon </a>
+          <p>
+            <i>* 36" tall x 15" wide <br/>
+              * mid summer to early fall <br/>
+              * $10.39 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/rudbeckia-fulgida-goldsturm"}>
+            BES - Black Eyed Susan
+          </a>
+          <p>
+            <i>* 24" tall x 24" wide<br/>
+              * yellow summer bloom <br/>
+              * $10.39 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/caryopteris-clandonensis-first-choice"}>
+            Spirea - First Choice Spirea
+          </a>
+          <p>
+            <i>
+              * 24 - 48" tall x 48" wide <br/>
+              * blooms early to late summer <br/>
+              * $11.19 each
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/helictotrichon-sempervirens"}>
+            Blue Oat Grass - Ornamental Grasses (Blue Avena)
+          </a>
+          <p>
+            <i>* 48" tall x 24" wide<br/>
+              * $11.19 each
+            </i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/penstemon-eatonii-richfield"}>
+            Penstemon - Firecracker Penstemon </a>
+          <p>
+            <i>* 36" tall x 15" wide <br/>
+              * mid summer to early fall <br/>
+              * $10.39 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/thymus-pink-chintz-nm"}>
+            ground cover - creeping thyme
+          </a>
+          <p>
+            <i>* 1" tall x 18" wide<br/>
+              * pink midspring <br/>
+              * $7.99 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/nassella-tenuissima"}>
+            STG - Mexican Feather Grass aka Silky Thread Grass
+          </a>
+          <p>
+            <i>* 18" tall x 12" wide<br/>
+              * $11.19 each</i>
+          </p>
+        </li>
+        <li>
+          <a href={"https://www.highcountrygardens.com/product/perennial-plants/echinacea-purpurea-pow-wow-white"}>
+            Pow wow echinacea
+          </a>
+          <p>
+            <i>* 20" tall x 16" wide<br/>
+              * white flowers summer <br/>
+              * $8.79 each</i>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+}
